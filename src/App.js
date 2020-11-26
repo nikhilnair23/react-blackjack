756ebd96faf73c905375ba1fba21bc29ee2e0393
nikhilnair23 <nikhilnair238@gmail.com>
2020-11-26 18:09:03 -0500

    Added ability to call for a hit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ const game = {
 
 function App() {
 
-
     const [currDeck, setDeck] = useState([]);
     const [playerScore, setPlayerScore] = useState(0);
     const [dealerScore, setDealerScore] = useState(0);
@@ -34,28 +33,70 @@ function App() {
         setDeck(deck);
     }
 
+    const reset = () => {
+        setDeck([]);
+        setGameState(game.INIT);
+        setPlayerHand([]);
+        setDealerHand([]);
+        setPlayerScore(0);
+        setDealerScore(0);
+    }
+
+    const dealCard = () => {
+        let hand = playerHand;
+        let card = currDeck.pop();
+        hand.push(card);
+        setPlayerHand([...hand]);
+        setDeck(currDeck);
+    }
+
+
     return (
         <div className="App">
             <div className="col p-4">
-                {gameState === game.INIT &&
-                <div className="btn btn-primary"
-                    onClick={startGame}
-                >
-                    Start Game
-                </div>
-                }
-                <div className="card-space">
+                <div className="playing-space">
                     <h3>Dealer</h3>
+                    <div className="playing-hand">
                     {dealerHand.map((card) =>
                     <Card card={card}/>
                     )}
+                    </div>
                 </div>
-                <div className="card-space">
+                <div className="playing-space">
                     <h3>Player</h3>
+                    <div className="playing-hand">
                     {playerHand.map((card) =>
                     <Card card = {card}/>
                     )}
+                    </div>
                 </div>
+
+                {gameState === game.STARTED &&
+                <div className="flex-row mb-2">
+                    <div className="btn btn-success mr-2"
+                         onClick={dealCard}
+                    >
+                        HIT
+                    </div>
+                    <div className="btn btn-danger">
+                        STAY
+                    </div>
+                </div>
+                }
+
+                {gameState === game.INIT ?
+                    <div className="btn btn-primary"
+                         onClick={startGame}
+                    >
+                        Start Game
+                    </div>
+                    :
+                    <div className="btn btn-primary"
+                         onClick={reset}
+                    >
+                        Restart
+                    </div>
+                }
             </div>
         </div>
     );
--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,7 @@
+import './card.css'
 export const Card = ({card}) => {
     return (
-    <div className="card">
+    <div className="card playing-card mr-2">
         <p>{card.suit}</p>
         <p>{card.value}</p>
     </div>
